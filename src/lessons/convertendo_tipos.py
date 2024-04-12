5dd21b8baf06546bc16fbb0b0b73ae2e8e4f4181
skywilian <codewilian852@gmail.com>
2024-04-12 16:04:59 -0300

    Criando um arquivo convertendo_tipos.py

---
+++ src/lessons/convertendo_tipos.py
@@ -0,0 +1,11 @@
+preco1 = 10
+print(f"{preco1} {type(preco1)}")
+
+preco2 = float(preco1)
+print(f"{preco2} {type(preco2)}")
+
+preco3 = '10'
+print(f"{preco3} {type(preco3)}")
+
+preco4 = int(preco3)
+print(f"{preco4} {type(preco4)}")
