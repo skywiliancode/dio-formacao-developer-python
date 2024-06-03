4502d063802c25bd22737bf203ec6d84e4d2b915
skywilian <codewilian852@gmail.com>
2024-06-03 11:06:12 -0300

    Entendendo sobre operadores de associação

---
+++ src/lessons/operadores_associacao.py
@@ -0,0 +1,9 @@
+# Operador de associação(in ou not in) verifica se o objeto esta numa sequência
+# A verificação e sensitive
+frutas = ["Manga", "Limão", "Uva"]
+
+# Laranja está na sequência de frutas?
+print("Laranja" in frutas)
+
+# Laranja não está na sequência de fruntas?
+print("Laranja" not in frutas)
