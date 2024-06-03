f9ba95e72a66d6709b9f8dee736f2151aa72f6da
skywilian <codewilian852@gmail.com>
2024-06-03 11:06:24 -0300

    Entendendo sobre operadores de atribuição

---
+++ src/lessons/operadores_atribuicao.py
@@ -0,0 +1,27 @@
+# Atribuição
+saldo = 500
+print(saldo)
+
+# Operador de adição
+saldo += 500
+print(saldo)
+
+# Operador de subtração
+saldo -= 500
+print(saldo)
+
+# Operador de multiplicação
+saldo *= 2
+print(saldo)
+
+# Operador de divisão
+saldo /= 100
+print(saldo)
+
+# Operador de potênciação
+saldo **= 2
+print(saldo)
+
+# SObreescrve a variável
+saldo = 250
+print(saldo)
