24c0d35dea5a7f481cdd779447b8e74535519dfb
skywilian <codewilian852@gmail.com>
2024-06-03 11:05:53 -0300

    Entendendo sobre operadores lógicos

---
+++ src/lessons/operadores_logicos.py
@@ -0,0 +1,26 @@
+# Operadores lógicos nas expressões retornam True(verdadeiro) ou False(Falso).
+
+saldo = 1000
+saque = 200
+limite = 100
+
+# Operador de maior que
+print(saldo > saque)
+
+# Operador de menor que
+print(saldo < saque)
+
+# Operador de maior que
+print(saldo > saque)
+
+# Operador de maior ou maior que
+print(saldo >= saque)
+
+# Operador de maior ou menor que
+print(saldo <= saque)
+
+# Outros opéradores lógicos:
+
+# Operador de negação = not
+# Operador ou = or
+# Operador e = and
